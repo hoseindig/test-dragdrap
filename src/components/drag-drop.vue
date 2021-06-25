efb2ebd86d607747b12bc0bd7c46f57148fc01a8
hoseindig <hosein_dig@yahoo.com>
2021-06-25 11:40:00 +0430

    first commit

---
+++ src/components/drag-drop.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <p style="position: absolute; bottom: 0px">
+      cordX : {{ cordX }} - cordY: {{ cordY }}
+    </p>
+    <h1>drag drop</h1>
+    <div id="mydiv" :style="{ top: cordY + 'px', left: cordX + 'px' }">
+      <div
+        id="mydivheader"
+        draggable="true"
+        @drag="dragging"
+        @dragstart="dragStart(1, $event)"
+        @dragover.prevent
+        @dragenter="dragEnter"
+        @dragleave="dragLeave"
+        @dragend="dragEnd"
+        @drop="dragFinish(1, $event)"
+      >
+        DRAG
+        <p>X : {{ cordX }} - Y: {{ cordY }}</p>
+      </div>
+      <p>BLA</p>
+      <p>BLA</p>
+      <p>BLA</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cordY: 0,
+      cordX: 0,
+    };
+  },
+  methods: {
+    dragging: function (e) {
+      // eslint-disable-next-line no-debugger
+      //   debugger;
+
+      this.cordY = e.clientY;
+      this.cordX = e.clientX;
+    },
+
+    dragStart(e) {
+      console.log("dragStart");
+      let obj = {
+        x: e.clientY,
+        y: e.clientX,
+      };
+      console.log("dragStart ", obj.x, obj.y);
+
+      //   ev.dataTransfer.setData("Text", this.id);
+      //   ev.dataTransfer.dropEffect = "move";
+      //   this.dragging = which;
+    },
+    dragEnter(e) {
+      // eslint-disable-next-line no-debugger
+      console.log("dragEnter");
+      let obj = {
+        x: e.clientY,
+        y: e.clientX,
+      };
+      console.log("dragEnter ", obj.x, obj.y);
+      /* 
+      if (ev.clientY > ev.target.height / 2) {
+        ev.target.style.marginBottom = '10px'
+      } else {
+        ev.target.style.marginTop = '10px'
+      }
+      */
+    },
+    dragLeave(e) {
+      // eslint-disable-next-line no-debugger
+      //   debugger;
+      console.log("dragLeave");
+      let obj = {
+        x: e.clientY,
+        y: e.clientX,
+      };
+      console.log("dragLeave ", obj.x, obj.y);
+      /* 
+      ev.target.style.marginTop = '2px'
+      ev.target.style.marginBottom = '2px'
+      */
+    },
+    dragEnd(e) {
+      console.log("dragEnd");
+      let obj = {
+        x: e.clientY - 100,
+        y: e.clientX - 70,
+      };
+      console.log("dragEnd ", obj.x, obj.y);
+      this.cordY = e.clientY;
+      this.cordX = e.clientX;
+      // eslint-disable-next-line no-debugger
+      //   debugger;
+      //   this.dragging = -1;
+    },
+    dragFinish(e) {
+      // eslint-disable-next-line no-debugger
+      //   debugger;
+      console.log("dragFinish");
+      let obj = {
+        x: e.clientY,
+        y: e.clientX,
+      };
+      console.log("dragFinish ", obj.x, obj.y);
+      //   this.moveItem(this.dragging, to);
+      //   ev.target.style.marginTop = "2px";
+      //   ev.target.style.marginBottom = "2px";
+    },
+  },
+};
+</script>
+
+<style scoped>
+#mydiv {
+  position: absolute;
+  width: 150px;
+  z-index: 9;
+  background-color: #f1f1f1;
+  border: 1px solid #d3d3d3;
+  text-align: center;
+}
+
+#mydivheader {
+  padding: 10px;
+  cursor: move;
+  z-index: 10;
+  background-color: #2196f3;
+  color: #fff;
+}
+</style>
+
+
---
+++ src/components/test-css.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <!-- <v-row>
+      <v-col class="col-md-6">
+        <v-text-field label="نام"> </v-text-field>
+      </v-col>
+    </v-row> -->
+    {{ sliderX }}
+    {{ sliderY }}
+    <p>sliderDXNum x :{{ obj.x }} y :{{ obj.y }}</p>
+    <v-row>
+      <v-col cols="12">
+        <v-slider
+          max="1000"
+          min="150"
+          v-model="sliderXNum"
+          thumb-label
+        ></v-slider>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-slider
+          max="1000"
+          min="150"
+          v-model="sliderYNum"
+          thumb-label
+        ></v-slider>
+      </v-col>
+    </v-row>
+    <v-layout justify-center>
+      <v-flex md5>
+        <v-text-field
+          href="#none"
+          label="نام"
+          :style="{ ...style, left: sliderX, top: sliderY }"
+        />
+        <input
+          type="text"
+          @click="calcPosOfBox"
+          ref="my_box_a"
+          value=""
+          placeholder="نام"
+          :style="{ ...style }"
+        />
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sliderXNum: 45,
+      sliderYNum: 45,
+      obj: {
+        x: null,
+        y: null,
+      },
+
+      //   items: [
+      //     {
+      //       key: "1",
+      //       name: "name",
+      //       text: "نام",
+      style: {
+        position: "absolute",
+        "font-size": "30px",
+        // left: "130px",
+        // top: "30px",
+      }, //'background-color':'red'
+      // },
+      //   ],
+    };
+  },
+  methods: {
+    calcPosOfBox() {
+      this.obj.x = this.$refs["my_box_a"].getBoundingClientRect().x;
+      this.obj.y = this.$refs["my_box_a"].getBoundingClientRect().y;
+      //   this.posBoxB = this.$refs["my_box_b"].getBoundingClientRect().x;
+    },
+    getPos() {
+      this.obj.x = this.$refs.busstop.getBoundingClientRect().left;
+      this.obj.y = this.$refs.busstop.getBoundingClientRect().top;
+    },
+  },
+  computed: {
+    sliderX() {
+      return this.sliderXNum + "px";
+    },
+    sliderY() {
+      return this.sliderYNum + "px";
+    },
+  },
+};
+</script>
